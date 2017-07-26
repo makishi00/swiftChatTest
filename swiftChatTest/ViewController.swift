0a88f82b84a95c997d7db9a9dd7f626a0719d33b
Makishi <makishi00@gmail.com>
2017-07-26 16:36:52 +0900

    init

---
+++ swiftChatTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftChatTest
+//
+//  Created by Makishi Mori on 2017/07/26.
+//  Copyright © 2017年 Makishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
